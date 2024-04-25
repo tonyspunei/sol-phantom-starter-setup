60c23fecfb8098299c0208f98d85d4ec34618108
Tony Spunei <tony.spunei@yahoo.com>
2024-04-25 14:16:34 +0200

    remove console.log

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -7,7 +7,6 @@ import SolanaLogo from "@/assets/solana-sol-logo.png"
 const WalletMultiButton = dynamic(() => import('@solana/wallet-adapter-react-ui').then((mod) => mod.WalletMultiButton), { ssr: false });
 
 export const AppBar: FC = () => {
-  console.log(WalletMultiButton)
   return (
     <div className="flex items-center justify-between bg-black py-5 px-10 w-full">
       <Image src={SolanaLogo} alt="Solana Logo" className="h-10 w-10" width={50} height={50} />
